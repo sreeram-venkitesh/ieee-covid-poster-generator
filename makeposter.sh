a3d9d934c49d5927a44ea71d6496ab9fd7babb12
NCM <neenuchacko@live.in>
2020-05-11 22:51:20 +0530

    Aligned all generated texts

--- makeposter.sh
+++ makeposter.sh
@@ -18,10 +18,10 @@ FIX="17:00 Hours"
 # district in the map The count of each district corresponds
 # to each district. 
 # Enter number of cases of each district in each variable here
-KASARGOD="(111)"
-KANNUR="(222)"
-KOZHIKODE="(330)"
-WAYANAD="(444)"
+KASARGOD=""
+KANNUR=""
+KOZHIKODE=""
+WAYANAD=""
 MALAPPURAM=""
 PALAKKAD=""
 THRISSUR=""
@@ -31,8 +31,10 @@ KOTTAYAM=""
 ALAPUZHA=""
 PATHANAMTHITTA=""
 KOLLAM=""
-THIRUVANANTHAPURAM=""
+TVM=""
 
+
+#THE X-Y CORDINATES ARE AS  -X 0 +X AND Y CORDINATES ARE INCREASING DOWNWARDS.
 
 # This command is used to enter a piece of text in the blank
 # image and produce an output image. Our plan of action is to
@@ -45,6 +47,9 @@ THIRUVANANTHAPURAM=""
 # specified. This coordinates have to selected according to the postiion
 # in the tempalte image. The source and destination image are specified
 # at the end 
+
+#TEXT GENERATED FOR THE STATES
+
 convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,180 '$DATE'" covid_blank.jpg output.jpg
 
 convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,215 '$FIX'" output.jpg output.jpg
@@ -56,6 +61,43 @@ convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "te
 convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -260,445  '$KOZHIKODE'" output.jpg output.jpg
 
 convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 10,340  '$WAYANAD'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -220,540  '$MALAPPURAM'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 140,548  '$PALAKKAD'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -140,650  '$THRISSUR'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 210,675  '$IDUKKI'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -125,755  '$ERNAKULAM'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -100,850  '$KOTTAYAM'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 270,940  '$PATHANAMTHITTA'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -80,925  '$ALAPUZHA'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -35,995  '$KOLLAM'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 300,1065  '$TVM'" output.jpg output.jpg
+
+#TEXT GENERATED FOR THE 4 STATUS
+
+ACTIVE=""
+REC=""
+DEAD=""
+CAS=""
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,455  '$ACTIVE'" output.jpg output.jpg
+
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,582  '$REC'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,698  '$DEAD'" output.jpg output.jpg
+
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,820  '$CAS'" output.jpg output.jpg
+
 
 # The following code is for inserting the an image, in this example 
 # the image of alapuzha, at the location specified by the coordinates
