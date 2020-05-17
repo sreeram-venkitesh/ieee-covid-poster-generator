4953ca113e8893357a793efdd3ca19d992eefe91
Anoop1507 <anoop.skumar1507@gmail.com>
2020-05-17 12:09:50 +0530

    Use PNG
    * No reason to use lossy jpg
    * Images are saved/modified multiple times resulting qulaity loss with jpg

--- makeposter.sh
+++ makeposter.sh
@@ -50,37 +50,37 @@ TVM=""
 
 #TEXT GENERATED FOR THE STATES
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,180 '$DATE'" covid_blank.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,180 '$DATE'" covid_blank.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,215 '$FIX'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 20 -gravity North -fill white -draw "text 440,215 '$FIX'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -370,230  '$KASARGOD'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -370,230  '$KASARGOD'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -310,345  '$KANNUR'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -310,345  '$KANNUR'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -260,445  '$KOZHIKODE'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -260,445  '$KOZHIKODE'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 10,340  '$WAYANAD'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 10,340  '$WAYANAD'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -220,540  '$MALAPPURAM'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -220,540  '$MALAPPURAM'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 140,548  '$PALAKKAD'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 140,548  '$PALAKKAD'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -140,650  '$THRISSUR'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -140,650  '$THRISSUR'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 210,675  '$IDUKKI'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 210,675  '$IDUKKI'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -125,755  '$ERNAKULAM'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -125,755  '$ERNAKULAM'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -100,850  '$KOTTAYAM'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -100,850  '$KOTTAYAM'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 270,940  '$PATHANAMTHITTA'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 270,940  '$PATHANAMTHITTA'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -80,925  '$ALAPUZHA'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -80,925  '$ALAPUZHA'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -35,995  '$KOLLAM'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text -35,995  '$KOLLAM'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 300,1065  '$TVM'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 300,1065  '$TVM'" output.png output.png
 
 #TEXT GENERATED FOR THE 4 STATUS
 
@@ -89,14 +89,14 @@ REC=""
 DEAD=""
 CAS=""
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,455  '$ACTIVE'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,455  '$ACTIVE'" output.png output.png
 
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,582  '$REC'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,582  '$REC'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,698  '$DEAD'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,698  '$DEAD'" output.png output.png
 
-convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,820  '$CAS'" output.jpg output.jpg
+convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "text 364,820  '$CAS'" output.png output.png
 
 
 # The following code is for inserting the an image, in this example 
@@ -105,4 +105,4 @@ convert -font ./fonts/Cantarell-Bold.otf -pointsize 23 -gravity North  -draw "te
 	
 x_alapuzha="450"
 y_alapuzha="200"
-#convert output.jpg \( ./images/alapuzha_g.png -background none -gravity center \) -gravity northwest -geometry +${x_alapuzha}+${y_alapuzha} -composite output.jpg
+#convert output.png \( ./images/alapuzha_g.png -background none -gravity center \) -gravity northwest -geometry +${x_alapuzha}+${y_alapuzha} -composite output.png
